ad6892da95fc37895da96aa60410e0bbe62a0b51
Benoit Ranque <ranque.benoit@gmail.com>
2023-09-29 11:50:11 -0400

    use .ts instead of .d.ts type files

--- typegen/typescript/index.ts
+++ typegen/typescript/index.ts
@@ -16,7 +16,7 @@ async function generate() {
   console.log("Generating types...");
   for (const schema of schemas) {
     writeFileSync(
-      `../generated/typescript/${schema}.d.ts`,
+      `../generated/typescript/${schema}.ts`,
       await compileFromFile(`../generated/json_schema/${schema}.schema.json`, {
         additionalProperties: false,
       })
